c8ee9d8201a292ae411ade3cac58d022836383cb
Rahul Monga <Rahulm5000@gmail.com>
2013-06-26 22:38:58 -0400

    fix article numbering

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -7,10 +7,7 @@ $(document).ready(function(){
       items.push('<tr><td>'+ x++ +'</td><td nowrap><a href="'+val.url +'">' + val.title + '</a></td><td nowrap>'+ val.publish_date +'</td></tr>');
     });
     items.push('</table>');
-    $('<ul/>', {
-      'class': 'my-new-list',
-      html: items.join('')
-    }).appendTo('#results');
+    $(items.join('')).appendTo('#results');
   });
 
   $.getJSON("http://api.feedzilla.com/v1/categories.json", function(data) {
@@ -39,14 +36,11 @@ function filter(id){
         var x     = 1;
         items.push('<table border="0"><tr><th>Num</th><th>Title</th><th>Date</th></tr>');
         $.each(data.articles, function(key, val) {
-          items.push('<tr><td>'+ x +'</td><td nowrap><a href="'+val.url +'">' + val.title + '</a></td><td nowrap>'+ val.publish_date +'</td></tr>');
+          items.push('<tr><td>'+ x++ +'</td><td nowrap><a href="'+val.url +'">' + val.title + '</a></td><td nowrap>'+ val.publish_date +'</td></tr>');
         });
         items.push('</table>');
         $('#results').empty();
-        $('<ul/>', {
-          'class': 'my-new-list',
-          html: items.join('')
-        }).appendTo('#results');
+        $(items.join('')).appendTo('#results');
       });
     }else{
       var tempurl = 'http://api.feedzilla.com/v1/categories/'+id+'/articles.json?count=31'
@@ -57,14 +51,11 @@ function filter(id){
 
         items.push('<table border="0"><tr><th>Num</th><th>Title</th><th>Date</th></tr>');
         $.each(data.articles, function(key, val) {
-          items.push('<tr><td>'+ x +'</td><td nowrap><a href="'+val.url +'">' + val.title + '</a></td><td nowrap>'+ val.publish_date +'</td></tr>');
+          items.push('<tr><td>'+ x++ +'</td><td nowrap><a href="'+val.url +'">' + val.title + '</a></td><td nowrap>'+ val.publish_date +'</td></tr>');
         });
         items.push('</table>');
         $('#results').empty();
-        $('<ul/>', {
-          'class': 'my-new-list',
-          html: items.join('')
-        }).appendTo('#results');
+        $(items.join('')).appendTo('#results');
       });
     }
     
